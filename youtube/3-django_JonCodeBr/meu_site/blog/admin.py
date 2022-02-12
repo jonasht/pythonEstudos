c1ba40fce90428ca1fe141445e7481492b270f55
jonasht <jhenriquet@outlook.com.br>
2022-02-12 12:30:28 -0300

    adding django lesson

--- youtube/3-django_JonCodeBr/meu_site/blog/admin.py
+++ youtube/3-django_JonCodeBr/meu_site/blog/admin.py
@@ -1,3 +1,17 @@
+from re import search
 from django.contrib import admin
+from .models import Post
+
+# registrando
+# admin.site.register(Post)
+
+# outra forma de registrar
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    # list_display = ('titulo', 'slug', 'autor', 'conteudo',)
+    list_display = ('titulo', 'autor', 'publicado', 'status')
+    search_fields = ('titulo', 'conteudo')
+    prepopulated_fields = {'slug': ('titulo',)}
 
 # Register your models here.
