25c777e6192543555ca09d0684d3a61e43e2996d
jonasht <jhenriquet@outlook.com.br>
2022-08-07 11:22:19 -0300

    adding new studies

--- sirus/django/django1/core/models.py
+++ sirus/django/django1/core/models.py
@@ -1,3 +1,20 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preco', decimal_places=2, max_digits=15)
+    estoque = models.IntegerField('qtd no estoque')
+    
+    def __str__(self) -> str:
+        return self.nome
+        
+class Cliente(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    sobrenome = models.CharField('sobrenome', max_length=100)
+    email = models.EmailField('email', max_length=150, unique=True)
+    data_nascimento = models.DateField('data nascimento', blank=True, null=True)
+
--- sirus/django/django1/core/views.py
+++ sirus/django/django1/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from urllib import request
+from django.shortcuts import render, HttpResponse
+from .models import Produto
 # Create your views here.
+
+def index(request):
+    produtos = Produto.objects.all().order_by('-id')
+
+    context = {
+        'meu_titulo': 'lista de produtos',
+        'produtos': produtos
+    }
+    return render(request, 'index.html', context)
+
