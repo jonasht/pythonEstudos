99de2734a459030579846989a69520b53be9a489
jonasht <jhenriquet@outlook.com.br>
2022-09-15 16:22:28 -0300

    rm

--- djangoTeste/3crud_cbv/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from core.models import Cliente
-
-# Create your views here.
-class ClienteList(ListView):
-    model = Cliente
-    queryset = Cliente.objects.all()
-    
