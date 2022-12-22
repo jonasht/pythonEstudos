5f3db332990021fbbf0821e6597cecddc6b5445a
jonasht <jhenriquet@outlook.com.br>
2022-12-22 15:31:41 -0300

    add

--- djangoTeste/3teste/main/urls.py
+++ djangoTeste/3teste/main/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('form/', form, name='form')
+    path('form/', form, name='form'),
+    path('create/', create, name='create'),
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    
+
+    
 ] 
